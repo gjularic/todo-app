911609baa0d55e4649013640af2ac4e4fe503498
gjularic <gvozdan.jularic@gmail.com>
2022-06-30 15:08:15 +0000

    Add 'user' and 'complete' values into models.py Todo class and migrate

--- todo/models.py
+++ todo/models.py
@@ -3,10 +3,20 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Todo(models.Model):
+
+    # delete tasks if user gets deleted
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    
+    complete = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+
+    # order tasks by 'complete' value
+    # push completed items to the bottom
+    class Meta:
+        ordering = ['complete']
