bda014bb1a65af851a1480abd9a2fa4befad2ec1
gjularic <gvozdan.jularic@gmail.com>
2022-06-30 14:58:03 +0000

    Create a table in todo_list.html file to render out todo list tasks

--- todo/models.py
+++ todo/models.py
@@ -6,6 +6,7 @@ class Todo(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return self.title
