1db2e52154d0dee841ebc40ef1d57f6024dd40e6
gjularic <gvozdan.jularic@gmail.com>
2022-06-30 16:11:52 +0000

    AddTodoDetail class in views.py and todo.detail.html template

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic.list import ListView
+from django.views.generic import ListView, DetailView
 from .models import Todo
 
 # Create your views here.
 class TodoList(ListView):
+    model = Todo
+    context_object_name = 'tasks'
+
+class TodoDetail(DetailView):
     model = Todo
     context_object_name = 'tasks'
