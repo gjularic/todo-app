3c282bf9816db094e238e6c09d5d8d994bed7e3f
gjularic <gvozdan.jularic@gmail.com>
2022-07-01 13:05:58 +0000

    Add register.html page template

--- todo/urls.py
+++ todo/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 from .views import TodoList, TodoDetail, TodoCreate, TodoUpdate, \
-    TodoDelete, TodoLogin
+    TodoDelete, TodoLogin, TodoRegister
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', TodoLogin.as_view(), name="login"),
-    
     # logout and send the user to login page
     path('logout/', LogoutView.as_view(next_page="login"), name="logout"),
-
+    path('register/', TodoRegister.as_view(), name="register"),
     path('', TodoList.as_view(), name="list"),
     path('detail/<int:pk>/', TodoDetail.as_view(), name="detail"),
     path('create/', TodoCreate.as_view(), name="create"),
