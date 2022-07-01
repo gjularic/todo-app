9d7fe464a57ef625f82281f8a180bfcd78f7ee75
gjularic <gvozdan.jularic@gmail.com>
2022-07-01 10:07:59 +0000

    Import LogoutView into urls.py and set up path to send the user to login page

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import TodoList, TodoDetail, TodoCreate, TodoUpdate, \
     TodoDelete, TodoLogin
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', TodoLogin.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(next_page="login"), name="logout"),
     path('', TodoList.as_view(), name="list"),
     path('detail/<int:pk>/', TodoDetail.as_view(), name="detail"),
     path('create/', TodoCreate.as_view(), name="create"),
